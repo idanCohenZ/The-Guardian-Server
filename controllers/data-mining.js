80d7aeb8689d60026caaa1f12206dabc04929f64
RoiYehezkel <roi5634@gmail.com>
2023-05-02 15:01:29 +0300

    delete print to console

--- controllers/data-mining.js
+++ controllers/data-mining.js
@@ -31,20 +31,17 @@ const setUpInstagram = async (token) => {
     // Got access token.
     accessToken = result.data.access_token;
     userId = result.data.user_id;
-    // console.log(accessToken);
-    // console.log(result.data);
 
     // get data from user account
     result = await axios.get(
       `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp&access_token=${accessToken}`
     );
-    // console.log(result.data);
     const posts_data = result.data;
     if (!posts_data) return console.log("No valid data found ");
     try {
       const user = new User({ user_id: userId, data: posts_data });
       const saved_data = await user.save();
-      console.log(`User has been added to the database ${saved_data}`);
+      // console.log(`User has been added to the database ${saved_data}`);
     } catch (error) {
       console.log(`error by creating new user ${error}`);
     }
