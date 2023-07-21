594d4f25966845ff9fd105aac322b27e4a3a57b9
idanCohenZ <101664812+idanCohenZ@users.noreply.github.com>
2023-07-21 12:51:20 +0300

    finished the semantics module

--- src/parsingData/SemanticSearch.js
+++ src/parsingData/SemanticSearch.js
@@ -1,3 +1,85 @@
+//new version - my version 20.07.23
+let natural = require("natural");
+const fs = require("fs");
+
+async function semanticCategoryCreationForArray(labelArrayObjects) {
+  let categoryResult;
+  let Wordsclassifier = new natural.BayesClassifier();
+
+  let category_array = [];
+  for (let index = 0; index < labelArrayObjects.length; index++) {
+    const element = labelArrayObjects[index];
+    categoryResult = await semanticCategoryCreation(
+      element.string,
+      Wordsclassifier
+    );
+    console.log(
+      "The category for: " + element.string + " is :" + categoryResult
+    );
+
+    if (categoryResult && !category_array.includes(categoryResult)) {
+      category_array.push(categoryResult);
+    }
+  }
+
+  console.log(
+    "finished semanticCategoryCreationForArray, the array of categories is: " +
+      category_array
+  );
+  return category_array;
+}
+
+async function semanticCategoryCreation(label, Wordsclassifier) {
+  await trainClassifier(Wordsclassifier);
+  return Wordsclassifier.classify(label);
+}
+
+async function trainClassifier(Wordsclassifier) {
+  Wordsclassifier.addDocument(
+    "Smile Light Happy Leisure Fun Cool Event ",
+    "General"
+  );
+  Wordsclassifier.addDocument(
+    "Hairstyle Bracelet Fashion accessory Cap Hat Body jewelry Jewellery Earrings making Sleeve Blazer T-shirt" +
+      "Shoe Sneakers Jeans Trousers Shorts Textile",
+    "A Fashion Enthusiast "
+  );
+  Wordsclassifier.addDocument(
+    " Field archery Arrow Target Elbow ",
+    "An Archer"
+  );
+  Wordsclassifier.addDocument(
+    " Road Mountain Path Boots Wood Bird Lake Tints and shades Branch Natural Twig landscape Asphalt Travel Terrestrial plant Tree Botany Grass Sunlight Lake Tints and shades Branch Natural Twig Grass Plant Flower Houseplant Flowerpot Cloud ",
+    " Hiking and Travelling"
+  );
+
+  Wordsclassifier.addDocument(" Font Words Typography Document", "Writing");
+
+  Wordsclassifier.addDocument(" Cigarette Smoke    ", "Smoker");
+
+  Wordsclassifier.addDocument(
+    "   Goggles Sunglasses  Vision care eye  ",
+    "Glasses- wearer"
+  );
+  Wordsclassifier.addDocument(
+    " Music guitar drums band flute trumpet Artist Musician Musical instrument Concert Microphone Guitarist String instrument   ",
+    "Music Lover"
+  );
+  Wordsclassifier.addDocument(
+    " Military camouflage uniform Marines Cargo pants Soldier Army Squad    ",
+    "Extolling Army Service"
+  );
+  Wordsclassifier.addDocument(
+    " Horse dog cat cow bird crow breed Whiskers Companion Snout Pet Retriever Carnivore   ",
+    "Animal Lover"
+  );
+
+  await Wordsclassifier.train();
+}
+module.exports = semanticCategoryCreationForArray;
+
+//--------------------------------------------------
+//The previous model - The one was saved on Roi's version:
 // let natural = require("natural");
 // const fs = require("fs");
 
@@ -81,28 +163,28 @@
 // }
 // module.exports = semanticCategoryCreationForArray;
 
-function semanticCategoryCreation(label) {
-  // i haven't subscribed to the api in rapid api but it would work if i do
-  //try and consoling the different parts - i believe the body is what we will need
+// function semanticCategoryCreation(label) {
+// i haven't subscribed to the api in rapid api but it would work if i do
+//try and consoling the different parts - i believe the body is what we will need
 
-  const axios = require("axios");
+//   const axios = require("axios");
 
-  const options = {
-    method: "GET",
-    url: `https://wordsapiv1.p.rapidapi.com/words/${label}/typeOf`,
-    headers: {
-      "X-RapidAPI-Key": "cddcfc5984msh4f6a815e3a56676p11a5f9jsn74d8dd7636af",
-      "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
-    },
-  };
+//   const options = {
+//     method: "GET",
+//     url: `https://wordsapiv1.p.rapidapi.com/words/${label}/typeOf`,
+//     headers: {
+//       "X-RapidAPI-Key": "cddcfc5984msh4f6a815e3a56676p11a5f9jsn74d8dd7636af",
+//       "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+//     },
+//   };
 
-  axios
-    .request(options)
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-}
-module.exports = semanticCategoryCreation;
+//   axios
+//     .request(options)
+//     .then(function (response) {
+//       console.log(response.data);
+//     })
+//     .catch(function (error) {
+//       console.error(error);
+//     });
+// }
+// module.exports = semanticCategoryCreation;
